e0a5ec1f79fdacded1b073c1e003c828ba198d94
icobhavesh <bhaveshsaxena058gmail.com>
2023-11-04 13:01:13 +0530

    change in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Home from './E-commerce/Home.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux/es/exports.js'
 // import Navbars from './Dummy.jsx'
 import { Store } from "../src/E-commerce/ReactRedux/CardSlice/Store.jsx"
-import Header from './Demoproject/Header.jsx'
-import Home_1 from './Demoproject/Home_1.jsx'
-import { NavBar } from './Demoproject/NavBar.jsx'
-import Hero from './Demoproject/Hero.jsx'
-import Motion from './Demoproject/Motion.jsx'
+// import Header from './Demoproject/Header.jsx'
+// import Home_1 from './Demoproject/Home_1.jsx'
+// import { NavBar } from './Demoproject/NavBar.jsx'
+// import Hero from './Demoproject/Hero.jsx'
+// import Motion from './Demoproject/Motion.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
